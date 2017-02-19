fced661d1a8baac49b9c8057d8858a321b39bac5
Juanlu <jlljunior_90@hotmail.com>
2017-02-19 20:30:16 +0100

    arreglado error de recogida del ratio de kpi, no estaba correctamente casteado a double

--- pai1Project/src/logic/KPIRecolect.java
+++ pai1Project/src/logic/KPIRecolect.java
@@ -25,7 +25,8 @@ public class KPIRecolect extends TimerTask{
 			int total = hashManager.getAllHashes().size();
 			int noIntegrity = hashManager.changedHashes().size();
 			int integrity = total-noIntegrity;
-			double ratio = integrity/total;
+			
+			double ratio = integrity/(total*1.);
 			KPI kpi = new KPI(0, ratio, integrity, noIntegrity, new Date());
 			kpiManager.addKPI(kpi);
 		} catch (NoSuchAlgorithmException | FileNotFoundException
